a44720575723bdd87983f3796579ebd01b72d6fe
gprmarquez <gprmarquez@gmail.com>
2021-04-29 00:42:03 -0700

    finished tailwindcss setup. Working on login form

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -20,7 +20,32 @@ export default function Login() {
 
   return (
     <div className="container flex mx-auto max-w-screen-md items-center h-screen">
-      <p>I have no idea.</p>
+      <div className="flex w-3/5">
+        <img
+          src="/images/iphone-with-profile.jpg"
+          alt="iPhone with Instagram open."
+        />
+      </div>
+      <div className="flex flex-col w-2/5">
+        <h1 className="flex justify-center w-full">
+          <img
+            src="/images/logo.png"
+            alt="Instagram"
+            className="mt-2 w-6/12 mb-4"
+          />
+        </h1>
+
+        {error && <p className="mb-4 text-xs text-red-primary">{error}</p>}
+
+        <form onSubmit={handleLogin} method="POST">
+          <input
+            aria-label="Enter your email address"
+            type="text"
+            placeholder="Email Address"
+            className="text-sm text-gray-base w-full mr-3 py-5 h-2"
+          />
+        </form>
+      </div>
     </div>
   );
 }
