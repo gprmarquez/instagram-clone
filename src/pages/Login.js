9182d647aa1302de2ae503c14c0ecfa269d67640
gprmarquez <gprmarquez@gmail.com>
2021-04-30 19:38:55 -0700

    added password field

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -41,8 +41,16 @@ export default function Login() {
           <input
             aria-label="Enter your email address"
             type="text"
-            placeholder="Email Address"
-            className="text-sm text-gray-base w-full mr-3 py-5 h-2"
+          placeholder="Email Address"
+            className="text-sm text-gray-base w-full mr-3 py-5 h-2 border border-gray-primary rounded mb-2"
+            onChange={({ target }) => console.log(target.value)}
+          />
+          <input
+            aria-label="Enter your password"
+            type="password"
+            placeholder="Password"
+            className="text-sm text-gray-base w-full mr-3 py-5 h-2 border border-gray-primary rounded mb-2"
+            onChange={({ target }) => console.log(target.value)}
           />
         </form>
       </div>
